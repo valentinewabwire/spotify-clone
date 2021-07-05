37e822d9143624bf79ab534fbdc767fc67bf9071
valentinewabwire <valentine.wabwire@tracom.co.ke>
2021-07-05 22:48:28 +0300

    Reducer introduction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+    }
+
+    console.log("I have a token :point", token);
+  }, []);
+  console.log("👲", user);
+
   return (
-    <div className="app">
-      <h1>I am test</h1>
-    </div>
+    //BEM
+    <div className="app">{token ? <Player /> : <Login />}</div>
   );
 }
 
